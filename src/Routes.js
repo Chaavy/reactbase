34de6f96c344a698ce976407591f3370ac9c478e
Javier Sierra <javiersierraju@hotmail.com>
2023-03-18 19:28:57 -0700

    Added new version for react-route-dom and updated Routes logic

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
+import { LoginPage } from "./pages/Login/LoginPage";
 import Movies from "./pages/Movies";
 
-const Routes = () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <HomePage />
-            </Route>
-            <Route exact path="/movies">
-                <Movies/>
-            </Route>
-            <Route exact path="/customers">
-                <h1>Customers Page</h1>
-            </Route>
-            <Route exact path="/diagrams">
-                <h1>Diagrams Page</h1>
-            </Route>
-        </Switch>
-    );
+const AppRouter = () => {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/customers" element={<><h1>Customers Component</h1></>} />
+          <Route path="/diagrams" element={<><h1>Diagrams Component</h1></>} />
+        </Routes>
+      </Layout>
+    </>
+    // <Switch>
+    //     <Route exact path="/">
+    //         <HomePage />
+    //     </Route>
+    //     <Route exact path="/movies">
+    //         <Movies/>
+    //     </Route>
+    //     <Route exact path="/customers">
+    //         <h1>Customers Page</h1>
+    //     </Route>
+    //     <Route exact path="/diagrams">
+    //         <h1>Diagrams Page</h1>
+    //     </Route>
+    // </Switch>
+  );
 };
 
-export default Routes;
+export default AppRouter;
