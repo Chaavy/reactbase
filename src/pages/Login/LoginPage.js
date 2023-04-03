bfac91f67f8cd079ee390766d3c3a13a0ebf9539
Pampis8 <juanjprm@gmail.com>
2023-04-02 19:27:49 -0600

    Bootstrap added to login

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -31,47 +31,58 @@ export const LoginPage = () => {
   const handleOnClickLogin = () =>
     GetAuthorizationAction(userName, password, dispatch)
       ? navigate('/home') : setShowErrorMessage(true);
-  
+
 
   return (
-    
 
-    <div className="Login">
-    <button onClick={()=> i18n.changeLanguage("es")}>ES</button>
-    <button onClick={()=> i18n.changeLanguage("en")}>EN</button>
-    <br/>
-    {t("header.hello-world")}
-    {showErrorMessage && (
-      <div className="successMessage">
-        {t("header.AutMessage")}
+    <form action="/action_page.php">
+
+      <div className="container mt-3">
+        <button className="btn btn-outline-primary btn-sm" onClick={() => i18n.changeLanguage("es")}>ES</button>
+        <button className='btn btn-outline-primary btn-sm' onClick={() => i18n.changeLanguage("en")}>EN</button>
       </div>
-    )}
-    <div className="LoginForm">
-      <div>
-      {t("header.UserNameText")}
+
+      <div className="container mt-3">
+        {t("header.hello-world")}
+        {showErrorMessage && (
+          <div className="successMessage">
+            {t("header.AutMessage")}
+          </div>
+        )}
+      </div>
+
+      <div className="mb-3 mt-3">
+        <label className="form-label">{t("header.UserNameText")}</label>
         <input
+          className="form-control"
           type="text"
           name="username"
           value={userName}
           onChange={handleUserNameChange}
         />
       </div>
-      <div>
-      {t("header.PasswordText")}
+
+      <div className="mb-3">
+        <label className="form-label">{t("header.PasswordText")}</label>
         <input
+          className="form-control"
           type="password"
           name="password"
           value={password}
           onChange={handlePasswordChange}
         />
       </div>
-      <div>
-        <button type="button" name="Login" onClick={handleOnClickLogin}>
-        {t("header.LoginText")}
-        </button>
+
+      <div className='form-check mb-3'>
+          <label className='form-check-label'>
+            <input class="form-check-input" type="checkbox"></input> Remembrer me
+          </label>
       </div>
-    </div>
-  </div>
+
+      <button className="btn btn-primary" type="button" name="Login" onClick={handleOnClickLogin}>
+        {t("header.LoginText")}
+      </button>
+    </form>
   )
 };
 
